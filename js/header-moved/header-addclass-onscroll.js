a058e534cd73a55c839966dce5bff4dba526f1cc
filippovskiy09 <arturfilippovskij@gmail.com>
2023-09-02 22:00:21 +0300

    update files

---
+++ js/header-moved/header-addclass-onscroll.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const header = document.querySelector('.header');
+	const addHeaderClass = () => header.classList.add('scroll')
+	const removeHeaderClass = () => header.classList.remove('scroll')
+
+	window.addEventListener('scroll', () => {
+		let scrollPos = window.scrollY
+
+		if(scrollPos >= 10) {
+			addHeaderClass();
+		}	else {
+			removeHeaderClass()
+		}
+	})
+})
